e30d53150e2a1f828ad731c7b9f1e08a2240c752
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2020-12-07 12:14:15 +0000

    Updating sandbox legis-graph

---
+++ code/javascript/example.js
@@ -0,0 +1,28 @@
+// npm install --save neo4j-driver
+// node example.js
+const neo4j = require('neo4j-driver');
+const driver = neo4j.driver('bolt://<HOST>:<BOLTPORT>',
+                  neo4j.auth.basic('<USERNAME>', '<PASSWORD>'), 
+                  {/* encrypted: 'ENCRYPTION_OFF' */});
+
+const query =
+  `
+  MATCH (s:State {code: $state})<-[:REPRESENTS]-(l:Legislator)
+  RETURN l.firstName+' '+l.lastName as name
+  `;
+
+const params = {"state": "NY"};
+
+const session = driver.session({database:"neo4j"});
+
+session.run(query, params)
+  .then((result) => {
+    result.records.forEach((record) => {
+        console.log(record.get('name'));
+    });
+    session.close();
+    driver.close();
+  })
+  .catch((error) => {
+    console.error(error);
+  });
---
+++ code/python/example.py
@@ -0,0 +1,23 @@
+# pip3 install neo4j-driver
+# python3 example.py
+
+from neo4j import GraphDatabase, basic_auth
+
+driver = GraphDatabase.driver(
+  "bolt://<HOST>:<BOLTPORT>", 
+  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
+
+cypher_query = '''
+MATCH (s:State {code: $state})<-[:REPRESENTS]-(l:Legislator)
+RETURN l.firstName+' '+l.lastName as name
+'''
+
+with driver.session(database="neo4j") as session:
+  results = session.read_transaction(
+    lambda tx: tx.run(cypher_query,
+      state="NY").data())
+
+  for record in results:
+    print(record['name'])
+
+driver.close()
